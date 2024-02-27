ff44691141efdf844594c6df51faccd462bbf0e4
Igor Veyner <igorveyner95@gmail.com>
2024-02-26 19:18:02 -0800

    feat: small change to title

--- app/navigation/title.tsx
+++ app/navigation/title.tsx
@@ -1,9 +1,8 @@
-import React from "react"
 
 export const Title = () => {
   return (
     <div className="flex items-center justify-center mt-10">
-      <div className="text-gray-100 font-bold">Personal Expense Tracker</div>
+      <div className="text-gray-100 font-bold italic">Personal Expense Tracker</div>
     </div>
   )
 }
